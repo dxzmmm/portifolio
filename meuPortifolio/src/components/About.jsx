1f98fcbae905a5e4175424fbf01b9e9d480b5368
Yuri Marlon <monnflavinp@gmail.com>
2025-09-18 20:53:59 -0300

    Adicionado header e about

--- meuPortifolio/src/components/About.jsx
+++ meuPortifolio/src/components/About.jsx
@@ -2,57 +2,111 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="bg-transparent ">
-      <div className="flex flex-col items-center justify-center px-6 py-20 text-center">
-        <div className="flex flex-col md:flex-row items-center gap-10 max-w-5xl mx-auto">
-          <img
-            src="/assets/img/imgYuri.png"
-            alt="avatar do yuri"
-            className="w-40 h-40 rounded-full border-purple-800 border bg-purple-300"
-          />
+    <div className="min-h-screen w-full bg-[#0f172a] relative">
+      {/* Dark Dotted Grid Background */}
+      <div className="min-h-screen w-full relative bg-black">
+        {/* Prismatic Aurora Burst - Multi-layered Gradient */}
+        <div
+          className="absolute inset-0 z-0"
+          style={{
+            background: `
+          radial-gradient(ellipse 120% 80% at 70% 20%, rgba(255, 20, 147, 0.15), transparent 50%),
+          radial-gradient(ellipse 100% 60% at 30% 10%, rgba(0, 255, 255, 0.12), transparent 60%),
+          radial-gradient(ellipse 90% 70% at 50% 0%, rgba(138, 43, 226, 0.18), transparent 65%),
+          radial-gradient(ellipse 110% 50% at 80% 30%, rgba(255, 215, 0, 0.08), transparent 40%),
+          #000000
+        `,
+          }}
+        />
 
-          <div className="flex-1">
-            <div className="flex flex-col">
-              <div>
-                <h1 className="flex">Fala eae!</h1>
-              </div>
-              <div className="flex">
-                <h1 className="text-4xl md:text-6xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-white via-purple-300 to-pink-300">
-                  Eu me chamo Yuri
-                </h1>
-                <p className=" flex justify-center text-8xl">✨</p>
-              </div>
-            </div>
-            <p className="text-xl text-gray-300 mb-6">
-              A fullstack developer with solid foundations in design.
-            </p>
-            <p className="text-lg text-gray-400 mb-8">
-              Passionate about crafting seamless user experiences at the
-              intersection of creativity and functionality.
-            </p>
+        <div className="relative z-10 flex flex-col items-center justify-center py-20  text-center h-dvh">
+          <div className="flex flex-col md:flex-row items-center gap-10  mx-10">
+            <img
+              src="/assets/img/imgYuri.png"
+              alt="avatar do yuri"
+              className="w-40 h-40 rounded-full border-purple-800 border bg-purple-300"
+            />
 
-            <div className="flex flex-wrap gap-4 justify-center">
-              <button className="px-6 py-3 bg-gray-900 hover:bg-gray-800 text-white rounded-lg transition-all duration-300 transform hover:scale-105 border border-purple-500">
-                Contact Me
-              </button>
-              <button className="px-6 py-3 bg-gray-900 hover:bg-gray-800 text-white rounded-lg transition-all duration-300 transform hover:scale-105 border border-purple-500">
-                View Projects
-              </button>
+            <div className="flex-col flex gap-3">
+              <div className="flex flex-col">
+                <div>
+                  <h1 className="flex font-poppins text-white font-semibold text-md pl-1">
+                    Fala eae!
+                  </h1>
+                </div>
+                <div className="flex items-center gap-4">
+                  <h1 className="text-4xl md:text-6xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-white via-purple-300 to-pink-300">
+                    Eu sou o Yuri
+                  </h1>
+                </div>
+              </div>
+              <div className="flex flex-col justify-start gap-2 text-start">
+                <p className="text-2xl text-gray-300 font-poppins font-semibold ">
+                  Sou desenvolvedor Backend
+                </p>
+                <p className=" text-lg text-gray-400 mb-8 font-poppins">
+                  Entusiasta da tecnologia e apaixonado por criar soluções.{" "}
+                  <br />
+                  Vivendo em uma busca constante por conhecimento e inovação.
+                </p>
+              </div>
+
+              <div className="flex gap-15">
+                <div className="gap-5 flex">
+                  <button className="px-6 py-3 bg-gray-900 hover:bg-gray-800 text-white rounded-lg transition-all duration-300 transform hover:scale-105 cursor-pointer">
+                    Fale comigo!
+                  </button>
+                  <button className="px-6 py-3 bg-gray-900 hover:bg-gray-800 text-white rounded-lg transition-all duration-300 transform hover:scale-105  cursor-pointer">
+                    Veja meus projetos!
+                  </button>
+                </div>
+                <div class="flex gap-4 pt-3">
+                  <a href="https://github.com/dxzmmm">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="size-5 fill-white hover:fill-zinc-700 transition"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 .5C5.4 0 0 5.4 0 12c0 5.3 3.4 9.8 8.2 11.4.6.1.8-.3.8-.6v-2.1c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1.1-.8.1-.8.1-.8 1.2.1 1.9 1.3 1.9 1.3 1 1.8 2.8 1.3 3.5 1 .1-.7.4-1.3.8-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6.1-3.2 0 0 1-.3 3.3 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.7 1.6.3 2.8.1 3.2.9.8 1.3 1.9 1.3 3.1 0 4.8-2.8 5.7-5.5 6 .4.4.8 1 .8 2.1v3.2c0 .3.2.7.8.6C20.6 21.8 24 17.3 24 12c0-6.6-5.4-12-12-12z" />
+                    </svg>
+                  </a>
+
+                  <a href="https://www.instagram.com/dxzmmm/">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="size-5 fill-white hover:fill-pink-500 transition"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.406-11.845a1.44 1.44 0 1 0 0 2.881 1.44 1.44 0 0 0 0-2.881z" />
+                    </svg>
+                  </a>
+
+                  <a href="https://www.linkedin.com/in/yuri-marlon-260740203/">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="size-5 fill-white hover:fill-blue-600 transition"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 0 1-2.063-2.065 2.064 2.064 0 1 1 2.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* Rodapé / Redes sociais */}
-        <div className="mt-16 flex gap-6 text-sm">
-          <a href="#" className="hover:text-purple-400 transition-colors">
-            GitHub
-          </a>
-          <a href="#" className="hover:text-purple-400 transition-colors">
-            LinkedIn
-          </a>
-          <a href="#" className="hover:text-purple-400 transition-colors">
-            Twitter
-          </a>
+          {/* Rodapé / Redes sociais */}
+          <div className="mt-16 flex gap-6 text-sm">
+            <a href="#" className="hover:text-purple-400 transition-colors">
+              GitHub
+            </a>
+            <a href="#" className="hover:text-purple-400 transition-colors">
+              LinkedIn
+            </a>
+            <a href="#" className="hover:text-purple-400 transition-colors">
+              Twitter
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- meuPortifolio/src/components/Header.jsx
+++ meuPortifolio/src/components/Header.jsx
@@ -4,13 +4,13 @@ import { useState } from "react";
 
 const Header = () => {
   return (
-    <div className="flex bg-header gap-20">
+    <div className="flex bg-header gap-2 sm:gap-20">
       <div className="py-3">
-        <h1 className="text-2xl  ml-60 font-inter font-bold bg-gradient-to-b from-purple-400 to-purple-900 bg-clip-text text-transparent">
+        <h1 className="text-2xl lg:ml-20 ml-5 sm:ml-10 font-inter font-bold bg-gradient-to-b from-purple-400 to-purple-900 bg-clip-text text-transparent">
           Yuri Marlon
         </h1>
       </div>
-      <div className="flex justify-around w-md pt-3">
+      <div className="flex justify-around w-md pt-2.5">
         <Link to="/">
           <p className="text-sm font-poppins font-semibold text-texto hover:bg-zinc-800 p-2 transition-all duration-200 delay-120 rounded-sm">
             Home
