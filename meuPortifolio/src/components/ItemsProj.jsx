4803da047c0e4f4c12d44510f6d669a0157f8a19
Yuri Marlon <cqeyuri@gmail.com>
2025-09-18 21:01:21 -0300

    Adicionado o header e about

---
+++ meuPortifolio/src/components/ItemsProj.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const ItemsProj = () => {
+  return (
+    <div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem velit
+          amet non voluptas, laudantium autem temporibus incidunt dicta omnis,
+          impedit tenetur repellat nisi. Dolorem eum aut delectus accusantium
+          quod minus?
+        </p>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem velit
+          amet non voluptas, laudantium autem temporibus incidunt dicta omnis,
+          impedit tenetur repellat nisi. Dolorem eum aut delectus accusantium
+          quod minus?
+        </p>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem velit
+          amet non voluptas, laudantium autem temporibus incidunt dicta omnis,
+          impedit tenetur repellat nisi. Dolorem eum aut delectus accusantium
+          quod minus?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ItemsProj;
